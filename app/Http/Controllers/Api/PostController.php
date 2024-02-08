fde76f552cc3d881a1150cb6f71cd1d338630d0b
khoudia96gueye <gueyekhoudialivediakhou@gmail.com>
2024-02-08 17:41:15 +0000

    test

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -72,7 +72,7 @@ class PostController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show()
+    public function show(Post $post)
     {
         try {
             $post= Post::where('user_id', auth()->user()->id)->latest();
